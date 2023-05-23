c833962692ff5b13fa98cea54c6b0d1725f96a57
Isaac <zhaparovaziz03@gmail.com>
2023-05-23 03:24:40 +0300

    Реализация транзакций

--- StartCS_Exceptions/StartCS_Exceptions/Views/Windows/TransactionView/TransactionView.xaml.cs
+++ StartCS_Exceptions/StartCS_Exceptions/Views/Windows/TransactionView/TransactionView.xaml.cs
@@ -23,98 +23,348 @@ namespace StartCS_Exceptions.Views.Windows.TransactionView
     /// </summary>
     public partial class TransactionView : UserControl
     {
+        static ObservableCollection<Client> Clients; //= new ObservableCollection<Client>();
+        string path = @"..\Debug\Client.xml";
+
         public TransactionView()
         {
             InitializeComponent();
+            Clients = new ObservableCollection<Client>();
+            XmlDeserialize();
         }
 
-        static ObservableCollection<Client> Clients = new ObservableCollection<Client>();
-        string path = @"..\Debug\Client.xml";
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void XmlSerialize(ObservableCollection<Client> clients)
         {
-            XmlDeserialize(Clients);
-            SearchCommandMethod();
-            
-            void XmlDeserialize(ObservableCollection<Client> clients)
+            File.WriteAllText(path, String.Empty);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ObservableCollection<Client>));
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
             {
-                if (File.Exists(path))
+                xmlSerializer.Serialize(fs, clients);
+            }
+        }
+
+        void XmlDeserialize()
+        {
+            if (File.Exists(path))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(ObservableCollection<Client>));
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
                 {
-                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(ObservableCollection<Client>));
-                    using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                    ObservableCollection<Client> newClients = xmlSerializer.Deserialize(fs) as ObservableCollection<Client>;
+                    if (newClients != null)
                     {
-                        ObservableCollection<Client> newClients = xmlSerializer.Deserialize(fs) as ObservableCollection<Client>;
-                        if (newClients != null)
+                        foreach (Client client in newClients)
                         {
-                            foreach (Client client in newClients)
-                            {
-                                Clients.Add(client);
-                            }
+                            Clients.Add(client);
                         }
                     }
                 }
             }
+        }
 
-            void SearchCommandMethod()
+        void SearchClient()
+        {
+            foreach (Client client in Clients)
             {
-                foreach (Client client in Clients)
+                if (SearchBox.Text == client.ID)
                 {
-                    if (SearchBox.Text == client.ID)
+                    FoundBalanceBlock.Text = client.Bill;
+                    DepFoundBalanceBlock.Text = client.DepBill;
+                    if (client.DepBill == "Закрытый")
                     {
-                        FoundBalanceBlock.Text = client.Bill;
-                        DepFoundBalanceBlock.Text = client.DepBill;
-                        if (client.DepBill == "Закрытый")
-                        {
-                            OpenDepositButton.Content = "Открыть";
-                            OpenDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
-                            DepButton.Style = OpenDepositButton.TryFindResource("ButtonColorCloseStyle") as Style;
-                            DepFoundBalanceBlock.Background = new SolidColorBrush(Colors.DarkRed);
-                        }
-                        else
-                        {
-                            OpenDepositButton.Content = "Закрыть";
-                            OpenDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorCloseStyle") as Style;
-                            DepButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
-                            DepFoundBalanceBlock.Background = new SolidColorBrush(Colors.White);
-                        }
-
-                        if (client.Bill == "Закрытый")
-                        {
-                            OpenNonDepositButton.Content = "Открыть";
-                            OpenNonDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
-                            NonDepButton.Style = OpenDepositButton.TryFindResource("ButtonColorCloseStyle") as Style;
-                            FoundBalanceBlock.Background = new SolidColorBrush(Colors.DarkRed);
-                        }
-                        else
-                        {
-                            OpenNonDepositButton.Content = "Закрыть";
-                            OpenNonDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorCloseStyle") as Style;
-                            NonDepButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
-                            FoundBalanceBlock.Background = new SolidColorBrush(Colors.White);
-                        }
+                        OpenDepositButton.Content = "Открыть";
+                        OpenDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
+                        DepButton.Style = OpenDepositButton.TryFindResource("ButtonColorCloseStyle") as Style;
+                        DepFoundBalanceBlock.Background = new SolidColorBrush(Colors.DarkRed);
                     }
-                }
-
-                foreach (Client client in Clients)
-                {
-                    if (FromAccountTransaction.Text == client.ID && FromAccountTransaction.Text != String.Empty
-                        && ToAccountTransaction.Text != string.Empty)
+                    else
                     {
-                        FromIDNonDepositBox.Text = client.Bill;
-                        FromIDDepositBox.Text = client.DepBill;
+                        OpenDepositButton.Content = "Закрыть";
+                        OpenDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorCloseStyle") as Style;
+                        DepButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
+                        DepFoundBalanceBlock.Background = new SolidColorBrush(Colors.White);
                     }
-                }
 
-                foreach (Client client in Clients)
-                {
-                    if (ToAccountTransaction.Text == client.ID && ToAccountTransaction.Text != String.Empty
-                       && FromAccountTransaction.Text != String.Empty)
+                    if (client.Bill == "Закрытый")
                     {
-                        ToIDDepositBox.Text = client.DepBill;
-                        ToIDNonDepositBox.Text = client.Bill;
+                        OpenNonDepositButton.Content = "Открыть";
+                        OpenNonDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
+                        NonDepButton.Style = OpenDepositButton.TryFindResource("ButtonColorCloseStyle") as Style;
+                        FoundBalanceBlock.Background = new SolidColorBrush(Colors.DarkRed);
+                    }
+                    else
+                    {
+                        OpenNonDepositButton.Content = "Закрыть";
+                        OpenNonDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorCloseStyle") as Style;
+                        NonDepButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
+                        FoundBalanceBlock.Background = new SolidColorBrush(Colors.White);
                     }
                 }
             }
+
+            foreach (Client client in Clients)
+            {
+                if (FromAccountTransaction.Text == client.ID && FromAccountTransaction.Text != String.Empty
+                    && ToAccountTransaction.Text != string.Empty)
+                {
+                    FromIDNonDepositBox.Text = client.Bill;
+                    FromIDDepositBox.Text = client.DepBill;
+                }
+            }
+
+            foreach (Client client in Clients)
+            {
+                if (ToAccountTransaction.Text == client.ID && ToAccountTransaction.Text != String.Empty
+                   && FromAccountTransaction.Text != String.Empty)
+                {
+                    ToIDDepositBox.Text = client.DepBill;
+                    ToIDNonDepositBox.Text = client.Bill;
+                }
+            }
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            SearchClient();
+        }
+
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            FoundBalanceBlock.Background = new SolidColorBrush(Colors.White);
+            DepFoundBalanceBlock.Background = new SolidColorBrush(Colors.White);
+            OpenNonDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
+            OpenNonDepositButton.Content = "";
+            OpenDepositButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
+            OpenDepositButton.Content = "";
+            DepButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
+            NonDepButton.Style = OpenDepositButton.TryFindResource("ButtonColorOpenStyle") as Style;
+            FromIDNonDepositBox.Background = new SolidColorBrush(Colors.White);
+            FromIDDepositBox.Background = new SolidColorBrush(Colors.White);
+            ToIDNonDepositBox.Background = new SolidColorBrush(Colors.White);
+            ToIDDepositBox.Background = new SolidColorBrush(Colors.White);
+
+            SearchBox.Text = "";
+            FoundBalanceBlock.Text = "";
+            DepFoundBalanceBlock.Text = "";
+            NonDepAccountIDBlock.Text = "";
+            NonDepAmountBlock.Text = "";
+            DepAccountIDBlock.Text = "";
+            DepAmountBlock.Text = "";
+            FromAccountTransaction.Text = "";
+            ToAccountTransaction.Text = "";
+            FromIDNonDepositBox.Text = "";
+            FromIDDepositBox.Text = "";
+            ToIDNonDepositBox.Text = "";
+            ToIDDepositBox.Text = "";
+            TransactionAmountBlock.Text = "";
+            DepTransactionAmountBlock.Text = "";
+        }
+
+        private void NonDepButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Client client in Clients)
+            {
+                if (NonDepAccountIDBlock.Text == client.ID && client.Bill != "Закрытый")
+                {
+                    if (NonDepAmountBlock.Text != string.Empty)
+                    {
+                        int sums = int.Parse(client.Bill) + int.Parse(NonDepAmountBlock.Text);
+                        client.Bill = Convert.ToString(sums);
+                    }
+                }
+            }
+            SearchClient();
+            XmlSerialize(Clients);
+        }
+
+        private void DepButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Client client in Clients)
+            {
+                if (DepAccountIDBlock.Text == client.ID && client.DepBill != "Закрытый")
+                {
+                    if (DepAmountBlock.Text != string.Empty)
+                    {
+                        int sums = int.Parse(client.DepBill) + int.Parse(DepAmountBlock.Text);
+                        client.DepBill = Convert.ToString(sums);
+                    }
+                }
+            }
+            SearchClient();
+            XmlSerialize(Clients);
+        }
+
+        private void OpenNonDepositButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Client client in Clients)
+            {
+                if (SearchBox.Text == client.ID)
+                {
+                    if (client.Bill == "Закрытый")
+                    {
+                        client.Bill = "0";
+                    }
+                    else
+                    {
+                        if (MessageBox.Show("Внимание весь счёт будет обнулён! Вы уверены что хотите продолжить?", "Внимание",
+                            MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No) == MessageBoxResult.Yes)
+                        {
+                            client.Bill = "Закрытый";
+                        }
+                    }
+                }
+            }
+            SearchClient();
+            XmlSerialize(Clients);
+        }
+
+        private void OpenDepositButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Client client in Clients)
+            {
+                if (SearchBox.Text == client.ID)
+                {
+                    if (client.DepBill == "Закрытый")
+                    {
+                        client.DepBill = "0";
+                    }
+                    else
+                    {
+                        if (MessageBox.Show("Внимание весь депозитный счёт будет обнулён! Вы уверены что хотите продолжить?", "Внимание",
+                           MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No) == MessageBoxResult.Yes)
+                        {
+                            client.DepBill = "Закрытый";
+                        }
+                    }
+                }
+            }
+            SearchClient();
+            XmlSerialize(Clients);
+        }
+
+        private void SeacrchButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Client client in Clients)
+            {
+                if (FromAccountTransaction.Text == client.ID && FromAccountTransaction.Text != String.Empty
+                    && ToAccountTransaction.Text != string.Empty)
+                {
+                    FromIDNonDepositBox.Text = client.Bill;
+                    FromIDDepositBox.Text = client.DepBill;
+
+                    if (FromIDNonDepositBox.Text == "Закрытый")
+                    {
+                        FromIDNonDepositBox.Background = new SolidColorBrush(Colors.DarkRed);
+                    }
+                    else { FromIDNonDepositBox.Background = new SolidColorBrush(Colors.White); }
+
+                    if (FromIDDepositBox.Text == "Закрытый")
+                    {
+                        FromIDDepositBox.Background = new SolidColorBrush(Colors.DarkRed);
+                    }
+                    else { FromIDDepositBox.Background = new SolidColorBrush(Colors.White); }
+
+                }
+                else continue;
+            }
+
+            foreach (Client client in Clients)
+            {
+                if (ToAccountTransaction.Text == client.ID && ToAccountTransaction.Text != String.Empty
+                   && FromAccountTransaction.Text != String.Empty)
+                {
+                    ToIDDepositBox.Text = client.DepBill;
+                    ToIDNonDepositBox.Text = client.Bill;
+
+                    if (ToIDDepositBox.Text == "Закрытый") { ToIDDepositBox.Background = new SolidColorBrush(Colors.DarkRed); }
+                    else { ToIDDepositBox.Background = new SolidColorBrush(Colors.White); }
+
+                    if (ToIDNonDepositBox.Text == "Закрытый") { ToIDNonDepositBox.Background = new SolidColorBrush(Colors.DarkRed); }
+                    else { ToIDNonDepositBox.Background = new SolidColorBrush(Colors.White); }
+                }
+                else continue;
+            }
+        }
+
+        private static int minusClientBalance;
+        private static bool ExistToBill = true;
+        private static bool ExistToDepBill = true;
+        private static Client dataNonDepositClient;
+        private static Client dataDepositClient;
+
+        private void TransferButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Client client in Clients)
+            {
+                if (FromAccountTransaction.Text == client.ID && FromAccountTransaction.Text != String.Empty
+                    && ToAccountTransaction.Text != String.Empty)
+                {
+                    if (TransactionAmountBlock.Text != String.Empty)
+                    {
+                        int amount = int.Parse(TransactionAmountBlock.Text);
+                        int clientBalance = int.Parse(client.Bill) - amount;
+                        client.Bill = Convert.ToString(clientBalance);
+                        minusClientBalance = amount;
+
+                        dataNonDepositClient = client;
+                    }
+                }
+            }
+
+            foreach (Client client in Clients)
+            {
+                if (client.Bill == "Закрытый") { ExistToBill = false; }
+                if (ToAccountTransaction.Text == client.ID && ToAccountTransaction.Text != String.Empty
+                    && FromAccountTransaction.Text != String.Empty)
+                {
+                    if (TransactionAmountBlock.Text != String.Empty && client.Bill != "Закрытый")
+                    {
+                        int sums = int.Parse(client.Bill) + minusClientBalance;
+                        client.Bill = Convert.ToString(sums);
+                    }
+                    else { MessageBox.Show("Счёт клиента закрытый"); }
+                }
+            }
+            SearchClient();
+            XmlSerialize(Clients);
+        }
+
+        private void DepTransferButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Client client in Clients)
+            {
+                if (FromAccountTransaction.Text == client.ID && FromAccountTransaction.Text != String.Empty
+                    && ToAccountTransaction.Text != String.Empty)
+                {
+                    if (DepTransactionAmountBlock.Text != String.Empty && client.Bill != "Закрытый")
+                    {
+                        int amount = int.Parse(DepTransactionAmountBlock.Text);
+                        int clientBalance = int.Parse(client.DepBill) - amount;
+                        client.DepBill = Convert.ToString(clientBalance);
+                        minusClientBalance = amount;
+
+                        dataDepositClient = client;
+                    }
+                }
+            }
+
+            foreach (Client client in Clients)
+            {
+                if (client.DepBill == "Закрытый") { ExistToDepBill = false; }
+
+                if (ToAccountTransaction.Text == client.ID && ToAccountTransaction.Text != String.Empty
+                    && FromAccountTransaction.Text != String.Empty)
+                {
+                    if (DepTransactionAmountBlock.Text != String.Empty && client.DepBill != "Закрытый")
+                    {
+                        int sums = int.Parse(client.DepBill) + minusClientBalance;
+                        client.DepBill = Convert.ToString(sums);
+                    }
+                    else { MessageBox.Show("Депозитный счёт клиента закрытый"); }
+                }
+            }
+            SearchClient();
+            XmlSerialize(Clients);
         }
     }
 }
